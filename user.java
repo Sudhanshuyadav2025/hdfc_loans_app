41e1890c72792cf90f18faa340321c3e25a3ca7a
Sudhanshu <sudhanshu.yadav@iratechnologies.com>
2025-05-02 14:33:33 +0530

    bug fix -574

--- user.java
+++ user.java
@@ -5,8 +5,10 @@ sum = i+j;
 multi = i*j;
 int a = 2025;
 double d = 3424.43;
+integer i = 321.54;
 System.out.println(a);
 System.out.println(+sum);
 System.out.println(+multi);
 System.out.println(d);
+System.out.println(i);
 }
