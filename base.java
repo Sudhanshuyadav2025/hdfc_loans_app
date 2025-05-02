f1808d3328e8a637e516b6a249ba7256a8558395
Sudhanshu <sudhanshu.yadav@iratechnologies.com>
2025-05-02 13:05:28 +0530

    Base class modified

--- base.java
+++ base.java
@@ -1,4 +1,9 @@
-class base{
+class Base{
 this base class is the parent class of all java classes.
 but this file can be extended by any other java class.
 }
+class main extends Base {
+string str = Jandel singh;
+System.out.println(str);
+}
+
