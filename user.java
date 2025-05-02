b6f3f0830bb792b098fdf7f6388e085bbfdad7eb
Sudhanshu <sudhanshu.yadav@iratechnologies.com>
2025-05-02 14:54:19 +0530

    integer field add

--- user.java
+++ user.java
@@ -6,9 +6,11 @@ multi = i*j;
 int a = 2025;
 double d = 3424.43;
 integer i = 321.54;
+Integer h = 2321;
 System.out.println(a);
 System.out.println(+sum);
 System.out.println(+multi);
 System.out.println(d);
 System.out.println(i);
+System.out.println(h);
 }
