b8f628458a53f315d17609c32788d0fa9928ec1d
Sudhanshuyadav2025 <sudhanshu.yadav@iratechnologies.com>
2025-05-02 11:19:28 +0530

    Update emp.java

--- emp.java
+++ emp.java
@@ -1,1 +1,5 @@
-Hey! Ashok it school is one of the best platform to learn about git & github. 
+Hey! Ashokit school is one of the best platform to learn about git & github. 
+  class main{
+  String name = "sudhanshu";
+  System.out.println(name);
+}
