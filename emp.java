3edf290ba5d9c595c6d9cbda213a15974a70224f
Sudhanshu <sudhanshu.yadav@iratechnologies.com>
2025-05-02 12:10:51 +0530

    emp.java file updated

--- emp.java
+++ emp.java
@@ -2,6 +2,7 @@ Hey! Ashokit school is one of the best platform to learn about git & github.
   class main{
   String name = "Sudhanshu_singh";
   string str = "Harsh";
-  System.out.println(str);
+  int q = 21;
+  System.out.println(str+" "+q);
   System.out.println(name);
 }
