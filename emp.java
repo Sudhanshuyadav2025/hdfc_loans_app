48afac358388a8eb5e71dec8d12ea43303a8d6c8
Sudhanshu <sudhanshu.yadav@iratechnologies.com>
2025-05-03 14:04:21 +0530

    emp.java modified

--- emp.java
+++ emp.java
@@ -8,6 +8,23 @@ Hey! Ashokit school is one of the best platform to learn about git & github.
   --------------------------------
   --------------------------------
 
+    ############################
+    ##############################
+}3602f81
+ · 
+May 3, 2025
+
+17 lines (14 loc) · 463 Bytes
+Hey! Ashokit school is one of the best platform to learn about git & github. 
+  class main{
+  String name = "Sudhanshu_singh";
+  string str = "Harsh";
+  int q = 21;
+  System.out.println(str+" "+q);
+  System.out.println(name);
+  --------------------------------
+  --------------------------------
+
     ##############################
     ##############################
     
@@ -15,3 +32,4 @@ Hey! Ashokit school is one of the best platform to learn about git & github.
     @@@@@@@@@@@@@@@@@@@@@@@@@@@
     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 }
+
